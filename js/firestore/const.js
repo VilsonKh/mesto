ed984ca2924253d61ca5056b49d1f8a9c0cc106b
Vilson <khvilson@yandex.ru>
2023-06-28 16:16:49 +0300

    bug fixed

---
+++ js/firestore/const.js
@@ -0,0 +1,6 @@
+export const API_KEY = "AIzaSyDDBt4i6-ETU2Shu6XNhCkvimsRHfdGe1A";
+export const AUTH_DOMAIN = "mesto-project-cae47.firebaseapp.com";
+export const PROJECT_ID = "mesto-project-cae47";
+export const STORAGE_BUSKET = "mesto-project-cae47.appspot.com";
+export const MESSAGING_SENDER_ID = "1020488787603";
+export const APP_ID = "1:1020488787603:web:49ea097949b415c1aa4f66";
